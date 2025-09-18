936953e310489dcdcd431561b5d8dbaedf3ac258
Zach Peletz <zach@turningpointstudio.com>
2025-09-18 13:03:53 -0400

    fix

--- bootsrap/bootstrap.cmd
+++ bootsrap/bootstrap.cmd
@@ -1,4 +1,4 @@
 @echo off
 echo Bootstrapping machine...
-powershell -ExecutionPolicy Bypass -NoProfile -Command "irm https://raw.githubusercontent.com/turningpointstudio/zephyr-provision/main/ConfigureWindowsForAnsible.ps1 | iex"
+powershell -ExecutionPolicy Bypass -NoProfile -Command "irm https://raw.githubusercontent.com/turningpointstudio/zephyr-provision/main/bootsrap/ConfigureWindowsForAnsible.ps1 | iex"
 pause
